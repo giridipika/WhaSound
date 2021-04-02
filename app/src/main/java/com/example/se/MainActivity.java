d3115e2a388c7e40bfb38a16d6183e6ed2ba48a3
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2021-04-01 23:16:41 -0500

    Sign up view, loaded. New View added on AndroidManifest.xml

--- app/src/main/java/com/example/se/MainActivity.java
+++ app/src/main/java/com/example/se/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.se;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    // This code opens new activity when Sign up is clicked
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.login_signup_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Sign_up_activity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/se/Sign_up_activity.java
@@ -0,0 +1,12 @@
+package com.example.se;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_up_activity extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+    }
+}
