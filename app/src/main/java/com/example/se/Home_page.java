393ccf2135f3f96d4d141f92a156d0efe9971f35
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2021-04-07 23:30:30 -0500

    Fixed backing up on homepage.

--- app/src/main/java/com/example/se/Home_page.java
+++ app/src/main/java/com/example/se/Home_page.java
@@ -6,6 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Home_page extends Sign_up_activity {
     // Sign_up activity extends MainActivity this should be resolved
+
+    // Disables back button on Home page; this means that the user cannot go to login page or sign up page
+    // Also cannot go back and forth in the tab structure
+    @Override
+    public void onBackPressed() {
+        // Do nothing
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
