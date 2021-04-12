5204de17465fcce1acb1ef726c9d27cb0bb99555
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2021-04-12 15:20:10 -0500

    Classify section started.

--- app/src/main/java/com/example/se/Classify.java
+++ app/src/main/java/com/example/se/Classify.java
@@ -1,18 +1,104 @@
 package com.example.se;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.net.URISyntaxException;
+
 public class Classify extends Fragment {
+//    private static final int RESULT_OK = 1; // 1 means true
+//    private static final String TAG = "File path";
+    private Button choose_file_button;
+//    private static final int FILE_SELECT_CODE = 0;
+    View classify_view;
+    // Will implement later from : https://github.com/iPaulPro/aFileChooser
+//
+//    private void showFileChooser() {
+//
+//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//        intent.setType("*/*");
+//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+//
+//        try {
+//            startActivityForResult(
+//                    Intent.createChooser(intent, "Select a File to Upload"),
+//                    FILE_SELECT_CODE);
+//        } catch (android.content.ActivityNotFoundException ex) {
+//            // Potentially direct the user to the Market with a Dialog
+//            // Provide the user with a toast to install a file manager
+//            Toast.makeText(getContext(),"Please install a File Manager.", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        switch (requestCode) {
+//            case FILE_SELECT_CODE:
+//                if (resultCode == RESULT_OK) {
+//                    // Get the Uri of the selected file
+//                    Uri uri = data.getData();
+//                    Log.d(TAG, "File Uri: " + uri.toString());
+//                    // Get the path
+//                    Context curr = classify_view.getContext();
+//                    String path = getPath(curr, uri);
+//                    Log.d(TAG, "File Path: " + path);
+//                    // Get the file instance
+//                    // File file = new File(path);
+//                    // Initiate the upload
+//                }
+//                break;
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
+//
+//    // This method gets the path for the file
+//    public static String getPath(Context context, Uri uri) throws URISyntaxException {
+//        if ("content".equalsIgnoreCase(uri.getScheme())) {
+//            String[] projection = { "_data" };
+//            Cursor cursor = null;
+//
+//            try {
+//                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+//                int column_index = cursor.getColumnIndexOrThrow("_data");
+//                if (cursor.moveToFirst()) {
+//                    return cursor.getString(column_index);
+//                }
+//            } catch (Exception e) {
+//                // Eat it
+//            }
+//        }
+//        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+//            return uri.getPath();
+//        }
+//
+//        return null;
+//    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.classify,container,false);
+        classify_view = inflater.inflate(R.layout.classify,container,false);
+        choose_file_button = (Button) classify_view.findViewById(R.id.classify_button);
+        choose_file_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return classify_view;
     }
 }
