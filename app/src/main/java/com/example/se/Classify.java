7c95c9edd736c888f52c977b1d81233f1de4c7f9
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2021-04-21 13:05:52 -0500

    Names of files changed; various threads added.
    Signed-off-by: Karun Dawadi <karun.dawadi@mavs.uta.edu>

--- app/src/main/java/com/example/se/Classify.java
+++ app/src/main/java/com/example/se/Classify.java
@@ -20,8 +20,12 @@ import androidx.fragment.app.Fragment;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.nio.MappedByteBuffer;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Classify extends Fragment {
     private Button choose_file_button;
@@ -29,23 +33,40 @@ public class Classify extends Fragment {
     private Uri fileUri;
     private String filePath; // This is the final file path
     View classify_view;
-    // To pass to the interpreter constructor
-    File newFile = new File("src/main/assets/soundclassifier.tflite");
+
+    // For live sound recognition
+    private static final int SAMPLE_RATE = 16000;
+    private static final int SAMPLE_DURATION_MS = 1000;
+    private static final int RECORDING_LENGTH = (int) (SAMPLE_RATE * SAMPLE_DURATION_MS / 1000);
+
+    // File inputs for tflite and labels.txt
+    private static final String LABEL_FILENAME = "file:///android_asset/conv_actions_labels.txt";
+    private static final String MODEL_FILENAME = "file:///android_asset/conv_actions_frozen.tflite";
+
+    // For recording
+    short[] recordingBuffer = new short[RECORDING_LENGTH];
+
+    // Various threads used for recognition and recording
+    int recordingOffset = 0;
+    boolean shouldContinue = true;
+    private Thread recordingThread; // Thread used for recording
+    boolean shouldContinueRecognition = true; // To check for recognition
+    private Thread recognitionThread; // Thread used for recognition
+    private final ReentrantLock recordingBufferLock = new ReentrantLock();
+    private final ReentrantLock tfLiteLock = new ReentrantLock();
+    private List<String> labels = new ArrayList<String>();
+    private List<String> displayedLabels = new ArrayList<>();
+
+    // For machine learning model
+    private final Interpreter.Options tfLiteOptions = new Interpreter.Options();
+    private MappedByteBuffer tfLiteModel;
+    private Interpreter tfLite;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         classify_view = inflater.inflate(R.layout.classify,container,false);
         choose_file_button = (Button) classify_view.findViewById(R.id.classify_button);
-
-        try {
-            Interpreter interpreter = new Interpreter(newFile);
-            Map<String, Object> inputs = new HashMap<>();
-            Map<String, Object> outputs = new HashMap<>();
-            interpreter.runSignature(inputs, outputs, "mySignature");
-            interpreter.close();
-        } catch (Exception E){
-
-        }
 
         choose_file_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,7 +78,7 @@ public class Classify extends Fragment {
                 // File path working.
                 /*
                     For pie chart we can have : https://github.com/PhilJay/MPAndroidChart
-                 * */
+                **/
             }
         });
         return classify_view;
