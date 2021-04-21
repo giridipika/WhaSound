e1e576d2aab97cf49b1c1d450a4b3b128ca7d016
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2021-04-21 12:38:14 -0500

    Try catch block added, labels.txt and tflite file added.
    Signed-off-by: Karun Dawadi <karun.dawadi@mavs.uta.edu>

--- app/src/main/java/com/example/se/Classify.java
+++ app/src/main/java/com/example/se/Classify.java
@@ -12,12 +12,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import org.tensorflow.lite.Interpreter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.File;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Classify extends Fragment {
     private Button choose_file_button;
@@ -25,12 +29,23 @@ public class Classify extends Fragment {
     private Uri fileUri;
     private String filePath; // This is the final file path
     View classify_view;
-
+    // To pass to the interpreter constructor
+    File newFile = new File("src/main/assets/soundclassifier.tflite");
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         classify_view = inflater.inflate(R.layout.classify,container,false);
         choose_file_button = (Button) classify_view.findViewById(R.id.classify_button);
+
+        try {
+            Interpreter interpreter = new Interpreter(newFile);
+            Map<String, Object> inputs = new HashMap<>();
+            Map<String, Object> outputs = new HashMap<>();
+            interpreter.runSignature(inputs, outputs, "mySignature");
+            interpreter.close();
+        } catch (Exception E){
+
+        }
 
         choose_file_button.setOnClickListener(new View.OnClickListener() {
             @Override
