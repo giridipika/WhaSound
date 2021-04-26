b5968757eff4d3f1b612d0448f08854e2634627d
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2021-04-26 16:02:06 -0500

    Everything works so far.
    Signed-off-by: Karun Dawadi <karun.dawadi@mavs.uta.edu>

--- app/src/main/java/com/example/se/Classify.java
+++ app/src/main/java/com/example/se/Classify.java
@@ -2,6 +2,8 @@ package com.example.se;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,20 +19,41 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class Classify extends Fragment {
     private Button choose_file_button;
+    private Button stop_classify;
     public static final int PICKFILE_RESULT_CODE = 1;
     private Uri fileUri;
     private String filePath; // This is the final file path
     View classify_view;
 
+    // To load from asset folder
+    private static final String LABEL_FILENAME = "file:///android_asset/conv_actions_labels.txt";
+    private static final String MODEL_FILENAME = "file:///android_asset/conv_actions_frozen.tflite";
+    private static final String LOG_TAG = "Error";
+
+    // For machine learning
+    private final Interpreter.Options tfliteOptions = new Interpreter.Options();
+    private MappedByteBuffer tfLiteModel;
+    private Interpreter tfLite;
+    private RecognizeCommands recognizeCommands = null;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         classify_view = inflater.inflate(R.layout.classify,container,false);
+
+        // Both finds the classify and stop classify button
         choose_file_button = (Button) classify_view.findViewById(R.id.classify_button);
+        stop_classify = (Button) classify_view.findViewById(R.id.stop_classify);
 
         choose_file_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +62,8 @@ public class Classify extends Fragment {
                 chooseFile.setType("*/*");
                 chooseFile = Intent.createChooser(chooseFile, "Choose a file");
                 startActivityForResult(chooseFile, PICKFILE_RESULT_CODE);
+
+                // At this point we have the path of the file
                 // File path working.
                 /*
                     For pie chart we can have : https://github.com/PhilJay/MPAndroidChart
@@ -60,4 +85,16 @@ public class Classify extends Fragment {
                 break;
         }
     }
+
+    // This method loads the TF lite file
+    private static MappedByteBuffer loadModelFile(AssetManager assets, String modelFileName)
+            throws IOException{
+        AssetFileDescriptor fileDescriptor = assets.openFd(modelFileName);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,declaredLength);
+    }
+
 }
